2317a06b6eee56a931a44d1dd5226b2fe905b6f3
Giaruei <247658354@qq.com>
2023-12-23 21:53:28 +0800

    Migrate to MySQL

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
 const client = global.prismadb || new PrismaClient();
 if (process.env.NODE_ENV !== "production") global.prismadb = client;
 
